10c1dec6722634753428b45e3e795d11ba0607a6
zzhang87 <thomas.zz.zhang@gmail.com>
2016-12-08 16:15:03 -0600

    modified read files

--- cluster.py
+++ cluster.py
@@ -7,8 +7,11 @@ def cluster(in_path, out_path):
     out_path = out_path + 'cluster\\'
     if not os.path.isdir(out_path):
         os.mkdir(out_path)
-        
-    files = os.listdir(in_path)
+
+    files = []
+    for f in os.listdir(in_path):
+        if f.endswith('.JPG') or f.endswith('.jpg'):
+            files.append(f)
     
     K = 3
     attempts = 3
@@ -16,27 +19,25 @@ def cluster(in_path, out_path):
     init = cv2.KMEANS_PP_CENTERS
 
     for f in files:
-        ext = f[-3:]
-        if ext == 'JPG' or ext == 'jpg':
-            print(f)
-            src = cv2.imread(in_path + f)
-            shape = np.shape(src)
-            img = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-            img = np.float32(img)
-            img = cv2.GaussianBlur(img, (51,51), 0)
-            img = img.reshape(shape[0] * shape[1], shape[2])
+        print(f)
+        src = cv2.imread(in_path + f)
+        shape = np.shape(src)
+        img = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+        img = np.float32(img)
+        img = cv2.GaussianBlur(img, (51,51), 0)
+        img = img.reshape(shape[0] * shape[1], shape[2])
 
-            _,labels,centers = cv2.kmeans(img, K, criteria, attempts, init)
+        _,labels,centers = cv2.kmeans(img, K, criteria, attempts, init)
 
-            clustered_imgs = np.zeros((K,shape[0],shape[1],shape[2]),np.uint8)
-            labels = labels.reshape(shape[0], shape[1])            
-            for i in xrange(shape[0]):
-                for j in xrange(shape[1]):
-                    k = labels[i][j]
-                    clustered_imgs[k][i][j] = src[i][j]
+        clustered_imgs = np.zeros((K,shape[0],shape[1],shape[2]),np.uint8)
+        labels = labels.reshape(shape[0], shape[1])            
+        for i in xrange(shape[0]):
+            for j in xrange(shape[1]):
+                k = labels[i][j]
+                clustered_imgs[k][i][j] = src[i][j]
 
-            for k in xrange(K):
-                cv2.imwrite(out_path + f[:-4] + '_' + str(k) + '.jpg', clustered_imgs[k])
+        for k in xrange(K):
+            cv2.imwrite(out_path + f[:-4] + '_' + str(k) + '.jpg', clustered_imgs[k])
 
     print('Clustering finished!\n')
 
